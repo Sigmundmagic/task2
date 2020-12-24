4e33f80e432f024a51ae03d3b58dcf89d29d6132
Sigmundmagic <76521763+Sigmundmagic@users.noreply.github.com>
2020-12-24 19:32:32 +0300

    -
    2. написать программу, которая принимает имена двух файлов: референсного fasta файла и vcf файла, и возвращает файл fasta, в котором в последовательность внесены замены в соответствии с vcf файлом. Считать, что в vcf файле могут быть записаны только однонуклеотидные замены, которые не меняют длину последовательности.

---
+++ PythonApplicationTaskNumberTwo.py
@@ -0,0 +1,88 @@
+import array
+def isHeader(string):
+    string = string.strip()
+    if len(string) < 2:
+        return False
+    if string[0] == '#' and string[1] != '#':
+        return True
+    return False
+
+def replaceChar(string,index,old,new):
+    if string[index] != old:
+        return string
+    arr = array.array('u',string)
+    arr.insert(index,new)
+    string = arr.tounicode()
+    return string
+
+def isComment(string):
+    string = string.strip()
+    if len(string) < 2:
+        return False
+    if string[0] == '#' and string[1] == '#':
+        return True
+    return False
+
+def raplaceInlistForFasta(name,pos,REF,ALT):
+    global listForFasta
+    for item in listForFasta:
+        if item.one == name and len(item.two) >= pos and item.two[pos] == REF:
+            item.two = replaceChar(item.two,pos,REF,ALT)#item.two[pos] = ALT
+
+class strFormVCFTable():
+    def __init__(self,string):
+        self.CHROM = string.split('\t')[0].strip()
+        self.POS = int(string.split('\t')[1].strip())
+        self.REF = string.split('\t')[3].strip()
+        self.ALT = string.split('\t')[4].strip()
+
+def updateListVCF():
+    global fileVCF
+    line = fileVCF.readline()
+    while True:
+        if isHeader(line) == True or isComment(line) == True:
+            line = fileVCF.readline()
+            if len(line) == 0:
+                break
+            continue
+        itemFromVCF = strFormVCFTable(line)
+        raplaceInlistForFasta(itemFromVCF.CHROM,itemFromVCF.POS,itemFromVCF.REF,itemFromVCF.ALT)
+        line = fileVCF.readline()
+        if len(line) == 0:
+            break
+
+def setResultFile():
+    global listForFasta
+    fileResult = open('result.fasta','w')
+    listForFile = []
+    for item in listForFasta:
+        listForFile.append('>' + item.one + '\n')
+        listForFile.append(item.two + '\n')
+    fileResult.writelines(listForFile)
+    fileResult.close()
+
+class Pair:
+    def __init__(self):
+        self.one = ''
+        self.two = ''
+    
+listForFasta = []
+f = open('1.fasta','r')
+line = f.readline().strip()
+pair = Pair()
+while True:
+    if line.find('>') != -1:
+        pair.one = line[line.find('>')+1:]
+    else:
+        pair.two = line
+        listForFasta.append(pair)
+        pair = Pair()
+    line = f.readline().strip()
+    if len(line) == 0:
+        break
+f.close()
+
+fileVCF = open('2.vcf')
+updateListVCF()
+fileVCF.close()
+setResultFile()
